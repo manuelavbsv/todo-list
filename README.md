e855bc23468fdc49cba7736fd4915f0110e4ae5c
Manuela <manuelavbsv@gmail.com>
2025-08-31 10:03:30 -0300

    Projeto lista de tarefas

---
+++ README.md
@@ -0,0 +1,28 @@
+# ✅ Lista de Tarefas – Projeto To-do List
+
+Este é um projeto simples de uma **Lista de Tarefas (To-do List)**, feito com **HTML, CSS e JavaScript**, como parte do meu aprendizado em desenvolvimento Front-End.
+
+---
+
+## ✨ Funcionalidades
+
+- Adicionar tarefas
+- Remover tarefas individualmente
+- Marcar tarefas como concluídas (e removê-las com efeito visual)
+- Limpar todas as tarefas de uma vez
+
+---
+
+## 💻 Tecnologias usadas
+
+- HTML5
+- CSS3
+- JavaScript
+
+---
+
+## 🚀 Acesse o projeto publicado
+
+👉 [Clique aqui para acessar o site](https://manuelavbsv.github.io/todo-list/)  
+
+---
---
+++ script.js
@@ -0,0 +1,42 @@
+function adicionarTarefa() {
+  const input = document.getElementById("nova-tarefa");
+  const texto = input.value.trim();
+
+  if (texto === "") {
+    alert("Digite uma tarefa antes de adicionar.");
+    return;
+  }
+
+  const lista = document.getElementById("lista-tarefas");
+
+  const item = document.createElement("li");
+  item.innerHTML = `
+    <span>${texto}</span>
+    <div class="botoes">
+        <button onclick="concluirTarefa(this)">Concluir</button>
+        <button onclick="removerTarefa(this)">Remover</button>
+    </div>
+  `;
+
+  lista.appendChild(item);
+  input.value = "";
+}
+
+function removerTarefa(botao) {
+  const item = botao.closest("li");
+  item.remove();
+}
+
+function concluirTarefa(botao) {
+    const item = botao.closest("li")
+    item.style.opacity = "0.5";
+    item.style.textDecoration = "line-through";
+    setTimeout(()=> item.remove(), 500);
+}
+
+function limparTudo(){
+    const lista = document.getElementById("lista-tarefas");
+    if (confirm("Tem certeza que deseja remover todas as tarefas?")) {
+        lista.innerHTML = "";
+    }
+}
